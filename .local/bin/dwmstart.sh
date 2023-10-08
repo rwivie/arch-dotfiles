da17a5d1f838695ac2f3a7a908d3a9a9347aff1c
Ron Ivie <rwivie@gmail.com>
2023-10-08 09:45:38 -0500

    add .local/bin/dwmstart.sh

--- .local/bin/dwmstart.sh
+++ .local/bin/dwmstart.sh
@@ -10,7 +10,8 @@ numlockx on &
 mpDris2 &
 dunst -conf ~/.config/dunst/dunstrc &
 picom --config ~/.config/picom/picom.conf --daemon &
-(sleep 1; xautolock -detectsleep -time 10 -locker 'lock -n' -killtime 11 -killer 'systemctl suspend' -corners '+-00' -cornerdelay 15 -notify 15 -notifier "notify-send 'Screen will lock in 15 s'") &
+(sleep 1; xautolock -time 10 -locker lock -corners '+-00' -cornerdelay 15 -notify 15 -notifier "notify-send 'Screen will lock in 15 s'") &
+#(sleep 1; xautolock -detectsleep -time 10 -locker 'lock -n' -killtime 11 -killer 'systemctl suspend' -corners '+-00' -cornerdelay 15 -notify 15 -notifier "notify-send 'Screen will lock in 15 s'") &
 caffeine &
 sleep 15 && $HOME/.config/dwmblocks-async/build/dwmblocks &
 
