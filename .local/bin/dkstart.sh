d54413fd1d4ce3e15915020c7c8c488262eb6fd5
Ron Ivie <rwivie@gmail.com>
2023-11-18 10:52:41 -0600

    add .local/bin/dkstart.sh

---
+++ .local/bin/dkstart.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+nitrogen --restore &
+xrdb -load ~/.Xresources &
+$HOME/.config/dk/polybar/launch.sh &
+numlockx on &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+#xsettingsd &
+nm-applet &
+picom --config ~/.config/picom/picom.conf &
+dunst -conf ~/.config/dk/dunst/dunstrc &
+xautolock -time 10 -locker lock -corners '+-00' -cornerdelay 15 -notify 15 -notifier "notify-send 'Screen will lock in 15 s'" &
+blueman-applet &
+mpDris2 &
+pasystray --no-notify &
+birdtray &
+valent &
+#(sleep 5; $HOME/.config/dk/polybar/bar_toggle.sh) &
